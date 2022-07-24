e707d6cf6f25449e3b3dd87c2fd95206dbfae61b
Collin Meyer <collinmmeyer@gmail.com>
2022-07-24 17:33:16 -0500

    updated .gitignore

--- build/src/csv/csv.cpp.d
+++
@@ -1,6 +0,0 @@
-build/./src/csv/csv.cpp.o: src/csv/csv.cpp src/csv/csv.h \
-  src/str_tools/str_tools.h
-
-src/csv/csv.h:
-
-src/str_tools/str_tools.h:
--- build/src/str_tools/str_tools.cpp.d
+++
@@ -1,4 +0,0 @@
-build/./src/str_tools/str_tools.cpp.o: src/str_tools/str_tools.cpp \
-  src/str_tools/str_tools.h
-
-src/str_tools/str_tools.h:
